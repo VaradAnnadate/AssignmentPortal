cc94d23fdc6320a722e9dc09b8cc406f15c2b527
VaradAnnadate <varadannadate@gmail.com>
2026-09-23 09:48:06 +0530

    Added patch

--- app.js
+++ app.js
@@ -31,6 +31,23 @@ app.get('/assignments', async(req, res)=>{
     }
 })
 
+app.patch('/assignments/:id', async(req, res)=>{
+    try{
+        const { id } = req.params;
+        const result = await pool.query(`
+            UPDATE assignments SET submitted = true where id = $1
+            RETURNING * ;`, [id])
+        if (result.row.length === 0){
+            return res.status(404).json({err:"Request not Found"})
+        } else {
+            res.status(201).json(result.rows[0])
+        }
+    }catch(err){
+        console.log(err)
+        res.status(500).json({err:"Server Failed"})
+    }
+})
+
 
 app.listen(3000,() => {
     console.log('Server running on port 3000')
